2b88fc9b0a5f6e0d03df686c70cb95bd9b2c1d9c
Sunbohong <sunbohong@gmail.com>
2018-06-15 20:23:44 +0800

    Update default ignore count to zero

--- config.py
+++ config.py
@@ -5,10 +5,10 @@
 
 condition = '1'
 # 忽略 C++ 的异常
-IGNORE_CXX_COUNT = 5
+IGNORE_CXX_COUNT = 0
 IGNORE_CXX_CONDITION = condition
 
 
 # 忽略 OC 的异常
-IGNORE_OC_COUNT = 1
+IGNORE_OC_COUNT = 0
 IGNORE_OC_CONDITION = condition
